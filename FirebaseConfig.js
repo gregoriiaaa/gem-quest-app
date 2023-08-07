076939ffb964ee305c0726fae95264d8a2a1ca8a
Gregoria  Millensifer <gregoriiaaa@berkeley.edu>
2023-08-07 11:08:54 -0700

    working on user queries

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
+import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -15,3 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
