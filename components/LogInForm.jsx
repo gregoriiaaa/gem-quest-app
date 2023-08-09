9b7c71002f1b6d55958d3035523e8ab44543553b
Mayra M Zamudio <34254292+MayZamudio@users.noreply.github.com>
2023-08-09 12:37:05 -0700

    Mayra/profile (#8)
    * added navigation folder, renamed LogIn page to Layout Screen, and added to navigation folder
    
    * Log In screen
    
    * added alt text to image
    
    * Build Profile set Up
    
    * Build Profile and Profile

--- components/LogInForm.jsx
+++ components/LogInForm.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
 import { TextInput, Text, Button } from "react-native-paper";
 import { Formik } from "formik";
 import * as Yup from "yup";
+
 import authService from "../authService.jsx";
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
@@ -48,7 +49,8 @@ const TextInputLiveFeedback = ({
   );
 };
 
-const CreateAccountForm = ({ navigation }) => {
+const LogInForm = ({ navigation }) => {
+
   const initialValues = {
     email: "",
     password: "",
@@ -59,7 +61,9 @@ const CreateAccountForm = ({ navigation }) => {
       .email("Invalid email")
       .required("Berkeley email is required")
       .matches(/^.*@berkeley\.edu$/, 'Email must end with "@berkeley.edu"'),
+    password: Yup.string().required("Password is required"),
   });
+
 
   const handleSubmit = async (values) => {
     // await sleep(500);
@@ -88,12 +92,24 @@ const CreateAccountForm = ({ navigation }) => {
             formikKey="password"
             secureTextEntry={true}
           />
-          <Text style={styles.forgotPassword} textColor="#21005D">
-            Forgot password?
+          <Text style={styles.signUpText}>
+            Don't have an account?{" "}
+            <Text>
+              {/* Wrap only the "Sign Up" text with TouchableOpacity */}
+              <TouchableOpacity
+                onPress={() => navigation.navigate("Create Account")}
+              >
+                <Text style={styles.signUpLink}>Sign Up</Text>
+              </TouchableOpacity>
+            </Text>
           </Text>
           <View style={styles.buttonContainer}>
             <Button
-              onPress={formikProps.handleSubmit}
+              onPress={() => {
+                // formikProps.handleSubmit();
+                navigation.navigate("LayoutScreen");
+              }}
+
               mode="contained"
               style={styles.button}
               buttonColor="#21005D"
@@ -120,11 +136,17 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     color: "#56595D",
   },
-  forgotPassword: {
+  signUpText: {
+
     marginTop: 10,
     fontStyle: "italic",
     fontSize: "x-small",
   },
+  signUpLink: {
+    color: "#21005D",
+    textDecorationLine: "underline", // Add underline to indicate a link
+  },
+
   button: {
     marginVertical: 2,
     borderRadius: 5,
@@ -145,4 +167,5 @@ const styles = StyleSheet.create({
   },
 });
 
-export default CreateAccountForm;
+export default LogInForm;
+
