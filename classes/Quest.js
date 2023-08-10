fe1f9e8fdf4aa8fb2a1115b511e30f098b0d36a6
Gregoria  Millensifer <gregoriiaaa@berkeley.edu>
2023-08-09 18:41:43 -0700

    quest class wip

--- classes/Quest.js
+++ classes/Quest.js
@@ -1,1 +1,37 @@
-// this is where i will create the Quest class
+class Quest {
+  constructor(id, hostUid, date, location) {}
+
+  /** Converts the JavaScript User Class into a plain JavaScript User Object.
+   *    @note Use this function for when you want to send a user object to firebase.
+   *
+   *    @returns userObject
+   */
+  toPlainJavaScriptObject() {
+    const userObject = {
+      name: this.name ? this.name : "",
+      email: this.email ? this.email : "",
+      dateOfBirth: this.dateOfBirth ? this.dateOfBirth : "",
+      pronouns: this.pronouns ? this.pronouns : "",
+      ethnicId: this.ethnicId ? this.ethnicId : "",
+      genderId: this.genderId ? this.genderId : "",
+      personalityType: this.personalityType ? this.personalityType : "",
+      petsDescription: this.petsDescription ? this.petsDescription : "",
+      dietaryRestrictions: this.dietaryRestrictions
+        ? this.dietaryRestrictions
+        : "",
+      coffeeTeaBoba: this.coffeeTeaBoba ? this.coffeeTeaBoba : "",
+      comfortFood: this.comfortFood ? this.comfortFood : "",
+      movieShowBook: this.movieShowBook ? this.movieShowBook : "",
+      topMovie: this.topMovie ? this.topMovie : "",
+      topShow: this.topShow ? this.topShow : "",
+      musicTaste: this.musicTaste ? this.musicTaste : "",
+      topArtist: this.topArtist ? this.topArtist : "",
+    };
+    return userObject;
+  }
+}
+
+// const user = new User();
+// console.log(user.name);
+
+export default User;
