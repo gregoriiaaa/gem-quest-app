da6d7b798c63c09eb7d2c718ac1bc4e680c1f799
MayZamudio <mayra_z@berkeley.edu>
2023-08-10 02:23:00 -0700

    completed the Quests Home page

--- screens/Home.jsx
+++ screens/Home.jsx
@@ -1,21 +1,49 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import BottomNavigation from "../components/BottomNavigation";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+} from "react-native";
+import MyQuestsCards from "../components/MyQuestsCards";
+import AttendingQuestsCard from "../components/AttendingQuestsCard";
+import AvailableQuests from "../components/AvailableQuests";
+import DraftsCard from "../components/draftsCard";
+
+import { Searchbar } from "react-native-paper";
 
 const Home = () => {
+  const [searchQuery, setSearchQuery] = React.useState("");
+  const onChangeSearch = (query) => setSearchQuery(query);
   return (
-    <View style={styles.container}>
-      <Text>Welcome to the Home page!</Text>
-      {/* Add other content for the Home page here */}
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <MyQuestsCards />
+        <AttendingQuestsCard />
+        <Searchbar
+          placeholder="Search..."
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+        />
+        <AvailableQuests />
+        <DraftsCard />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    paddingTop: StatusBar.currentHeight,
+    // justifyContent: "center",
+    // alignItems: "center",
+  },
+  scrollView: {
+    marginHorizontal: 30,
+    marginVertical: 10,
   },
 });
 
